4dd47b90d60db5ad39e8caa16fe5fc63b675c94c
Lidiya666 <valeriya666@gmail.com>
2019-03-09 04:36:35 +0300

    lesson 3

---
+++ 1l_ChernovaVV/1l_ChernovaVV/Friends/PhotoFriendCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoFriendCollectionViewCell.swift
+//  1l_ChernovaVV
+//
+//  Created by Lidiya on 09/03/2019.
+//  Copyright © 2019 lidiya. All rights reserved.
+//
+
+import UIKit
+
+class PhotoFriendCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoFriend: UIImageView!
+}
